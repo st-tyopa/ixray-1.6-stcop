b5ef4d74b0bd0e8a1322f9b0b466369086c53b3d
mnelenpridumivat <ragrind@mail.ru>
2025-05-21 19:48:10 +0300

    Add window to show individual items in inventory

--- src/xrGame/ui/UIActorMenu.h
+++ src/xrGame/ui/UIActorMenu.h
@@ -44,6 +44,7 @@ enum EDDListType{
 		iDeadBodyBag,
 		iQuickSlot,
 		iTrashSlot,
+		iStackList,
 		iListTypeMax
 };
 
@@ -92,6 +93,7 @@ protected:
 
 	CUIDragDropListEx*			m_pInventoryBeltList;
 	CUIDragDropListEx*			m_pInventoryBagList;
+	CUIDragDropListEx*			m_pInventoryStackList;
 
 	CUIDragDropListEx*			m_pTradeActorBagList;
 	CUIDragDropListEx*			m_pTradeActorList;
@@ -269,12 +271,15 @@ protected:
 	bool				OnItemStartDrag				(CUICellItem* itm);
 	bool				OnItemDbClick				(CUICellItem* itm);
 	bool				OnItemSelected				(CUICellItem* itm);
+	bool				OnItemDeselected				(CUICellItem* itm);
 	bool				OnItemRButtonClick			(CUICellItem* itm);
 	bool				OnItemFocusReceive			(CUICellItem* itm);
 	bool				OnItemFocusLost				(CUICellItem* itm);
 	bool				OnItemFocusedUpdate			(CUICellItem* itm);
 	void				OnDragItemOnTrash			(CUIDragItem* item, bool b_receive);
 	bool						OnItemDropped				(PIItem itm, CUIDragDropListEx* new_owner, CUIDragDropListEx* old_owner);
+
+	void ActivateStackList(CUICellItem* cell_item);
 
 	void						ResetMode					();
 	void						InitInventoryMode			();
