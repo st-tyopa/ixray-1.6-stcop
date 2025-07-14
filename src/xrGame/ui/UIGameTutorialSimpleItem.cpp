1b24fad44a876e6177de26b9caf86a9db73e9baf
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-15 00:34:52 +0300

    Fix inventory not showing up

--- src/xrGame/ui/UIGameTutorialSimpleItem.cpp
+++ src/xrGame/ui/UIGameTutorialSimpleItem.cpp
@@ -14,6 +14,7 @@
 #include "../MainMenu.h"
 #include "../../xrScripts/script_engine.h"
 #include "../ai_space.h"
+#include "uiinventorywnd.h"
 
 extern ENGINE_API BOOL bShowPauseString;
 
@@ -210,6 +211,7 @@ void CUISequenceSimpleItem::Update()
 		{
 			if (CurrentGameUI()->PdaMenu().IsShown()		||
 				(&CurrentGameUI()->ActorMenu() && CurrentGameUI()->ActorMenu().IsShown())		||
+				(&CurrentGameUI()->InventoryWnd() && CurrentGameUI()->InventoryWnd().IsShown()) ||
 				CurrentGameUI()->TalkMenu->IsShown()			||
 				CurrentGameUI()->UIChangeLevelWnd->IsShown() ||
 				(MainMenu()->IsActive() && !m_owner->m_flags.test(CUISequencer::etsOverMainMenu) )
