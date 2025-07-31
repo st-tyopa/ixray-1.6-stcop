e13f8fe1e59b21e1acf954d70f6f84687836bc65
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-31 16:50:12 +0300

    Fix talk menu not showing up

--- src/xrGame/ui/UIActorMenu_action.cpp
+++ src/xrGame/ui/UIActorMenu_action.cpp
@@ -17,6 +17,8 @@
 #include "UIInventoryUtilities.h"
 #include "game_cl_base.h"
 
+#include "UITalkWnd.h"
+#include "UITalkDialogWnd.h"
 #include "../../xrUI/UICursor.h"
 #include "UICellItem.h"
 #include "UICharacterInfo.h"
@@ -449,6 +451,9 @@ bool CUIActorMenu::OnKeyboardAction(int dik, EUIMessages keyboard_action)
 		{
 			g_btnHint->Discard();
 			HideDialog();
+
+			if (m_pActorInvOwner->IsTalking())
+				CurrentGameUI()->TalkMenu->UITalkDialogWnd->Show();
 		}
 		return true;
 	}	
@@ -459,6 +464,9 @@ bool CUIActorMenu::OnKeyboardAction(int dik, EUIMessages keyboard_action)
 		{
 			g_btnHint->Discard();
 			HideDialog();
+
+			if (m_pActorInvOwner->IsTalking())
+				CurrentGameUI()->TalkMenu->UITalkDialogWnd->Show();
 		}
 		return true;
 	}
@@ -512,6 +520,9 @@ void CUIActorMenu::OnBtnExitClicked(CUIWindow* w, void* d)
 {
 	g_btnHint->Discard();
 	HideDialog();
+
+	if (m_pActorInvOwner->IsTalking())
+		CurrentGameUI()->TalkMenu->UITalkDialogWnd->Show();
 }
 
 void CUIActorMenu::OnMesBoxYes( CUIWindow*, void* )
