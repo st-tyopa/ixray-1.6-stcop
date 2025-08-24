88cc4734428878a1e99d7163042b7f3ba88ec19d
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-08-24 12:50:14 +0300

    Fix crash if upgrades_properties section not exist

--- src/xrGame/ui/UIInvUpgradeProperty.cpp
+++ src/xrGame/ui/UIInvUpgradeProperty.cpp
@@ -169,29 +169,30 @@ void UIInvUpgPropertiesWnd::init_from_xml( LPCSTR xml_name )
 
 	LPCSTR properties_section = "upgrades_properties";
 
-	VERIFY2( pSettings->section_exist( properties_section ), make_string<const char*>( "Section [%s] does not exist !", properties_section ) );
-	VERIFY2( pSettings->line_count( properties_section ), make_string<const char*>( "Section [%s] is empty !",       properties_section ) );
 	shared_str property_id;
 
-	CInifile::Sect&		inv_section = pSettings->r_section( properties_section );
-	CInifile::SectIt_	ib = inv_section.Data.begin();
-	CInifile::SectIt_	ie = inv_section.Data.end();
-	for ( ; ib != ie ; ++ib )
+	if (pSettings->section_exist(properties_section))
 	{
-		UIProperty* ui_property = new UIProperty(); // load one time !!
-		ui_property->init_from_xml( ui_xml );
-
-		property_id._set( (*ib).first );
-		if ( !ui_property->init_property( property_id ) )
+		CInifile::Sect& inv_section = pSettings->r_section(properties_section);
+		CInifile::SectIt_	ib = inv_section.Data.begin();
+		CInifile::SectIt_	ie = inv_section.Data.end();
+		for (; ib != ie; ++ib)
 		{
-			Msg( "! Invalid property <%s> in inventory upgrade manager!", property_id.c_str() );
-			xr_delete(ui_property);
-			continue;
-		}
+			UIProperty* ui_property = new UIProperty(); // load one time !!
+			ui_property->init_from_xml(ui_xml);
 
-		m_properties_ui.push_back( ui_property );
-		AttachChild( ui_property );
-	} // for ib
+			property_id._set((*ib).first);
+			if (!ui_property->init_property(property_id))
+			{
+				Msg("! Invalid property <%s> in inventory upgrade manager!", property_id.c_str());
+				xr_delete(ui_property);
+				continue;
+			}
+
+			m_properties_ui.push_back(ui_property);
+			AttachChild(ui_property);
+		} // for ib
+	}
 	ui_xml.SetLocalRoot( stored_root );
 }
 
