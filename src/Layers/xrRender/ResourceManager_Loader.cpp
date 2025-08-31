841657fc12624a4507911f988acebb8ce4adf39c
wh1t3lord <git.matvey@yandex.ru>
2025-08-31 11:35:56 +0300

    ADD: continue, but need to update branch;

--- src/Layers/xrRender/ResourceManager_Loader.cpp
+++ src/Layers/xrRender/ResourceManager_Loader.cpp
@@ -126,8 +126,9 @@ void	CResourceManager::OnDeviceCreate	(IReader* F)
 	// we don't use storage svg if rendering ui is raster because there's no need in such creation
 	if (!this->m_pStorageSVG && !EngineExternal().isRenderingUIRaster())
 	{
-		this->m_pStorageSVG = new CSVGStorage(eSVGStorageFlags::kFeatureSVGStorage_Static_Allocation);
-		this->m_pStorageSVG->init(RDevice, RContext);
+		R_ASSERT(false && "continue");
+	//	this->m_pStorageSVG = new CSVGStorage(eSVGStorageFlags::kFeatureSVGStorage_Static_Allocation);
+	//	this->m_pStorageSVG->init(RDevice, RContext);
 	}
 }
 
