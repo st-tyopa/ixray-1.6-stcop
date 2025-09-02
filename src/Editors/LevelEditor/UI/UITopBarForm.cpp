ixray-team/ixray-1.6

Fix PS\Shader\Level Editor compile

src/Editors/LevelEditor/UI/UITopBarForm.cpp
@@ -31,7 +31,7 @@ UITopBarForm::~UITopBarForm() {}
 
 #define IMGUI_HINT_BUTTON(Name, Ptr, Hint, Callback) \
 			Ptr->Load(); \
-			if (ImGui::ImageButton("##" Name, Ptr->pSurface, ImVec2(20, 20))) \
+			if (ImGui::ImageButton("##" Name, Ptr->get_SRView(), ImVec2(20, 20))) \
 				Callback(); \
 			if (ImGui::IsItemHovered()) \
 			{ \
@@ -42,7 +42,7 @@ UITopBarForm::~UITopBarForm() {}
 
 #define IMGUI_HINT_BUTTON_EX(Name, Ptr, Timer, Hint, Callback) \
 			Ptr->Load(); \
-			if (ImGui::ImageButton("##" Name, Ptr->pSurface, ImVec2(20, 20), ImVec2(Timer > EDevice->TimerAsync() ? 0.5 : 0, 0), ImVec2(m_timeUndo > EDevice->TimerAsync() ? 1 : 0.5, 1))) \
+			if (ImGui::ImageButton("##" Name, Ptr->get_SRView(), ImVec2(20, 20), ImVec2(Timer > EDevice->TimerAsync() ? 0.5 : 0, 0), ImVec2(m_timeUndo > EDevice->TimerAsync() ? 1 : 0.5, 1))) \
 			{ \
 				Callback(); \
 				Timer = EDevice->TimerAsync() + 130;\

st-tyopa/ixray-1.6-stcop

src/Editors/LevelEditor/UI/UITopBarForm.cpp
@@ -32,7 +32,7 @@ UITopBarForm::~UITopBarForm() {}
 
 #define IMGUI_HINT_BUTTON(Name, Ptr, Hint, Callback) \
 			Ptr->Load(); \
-			if (ImGui::ImageButton("##" Name, Ptr->pSurface, ImVec2(20, 20))) \
+			if (ImGui::ImageButton("##" Name, Ptr->get_SRView(), ImVec2(20, 20))) \
 				Callback(); \
 			if (ImGui::IsItemHovered()) \
 			{ \
@@ -43,7 +43,7 @@ UITopBarForm::~UITopBarForm() {}
 
 #define IMGUI_HINT_BUTTON_EX(Name, Ptr, Timer, Hint, Callback) \
 			Ptr->Load(); \
-			if (ImGui::ImageButton("##" Name, Ptr->pSurface, ImVec2(20, 20), ImVec2(Timer > EDevice->TimerAsync() ? 0.5 : 0, 0), ImVec2(Timer > EDevice->TimerAsync() ? 1 : 0.5, 1))) \
+			if (ImGui::ImageButton("##" Name, Ptr->get_SRView(), ImVec2(20, 20), ImVec2(Timer > EDevice->TimerAsync() ? 0.5 : 0, 0), ImVec2(Timer > EDevice->TimerAsync() ? 1 : 0.5, 1))) \
 			{ \
 				Callback(); \
 				Timer = EDevice->TimerAsync() + 130;\